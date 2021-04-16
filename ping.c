6c83f35c880dab2231e9bfb2d83b687c9020b332
Mohamad Hosein Hoseinee <smhhoseinee@gmail.com>
2021-04-16 16:27:15 +0430

    multi thread created but not tested

--- ping.c
+++ ping.c
@@ -18,6 +18,9 @@
 #include <signal.h>
 #include <time.h>
 #include <stdbool.h>
+#include <pthread.h>
+#include <assert.h>
+
 
 
 // Automatic port number
@@ -51,6 +54,16 @@ struct ping_pkt
 	//char *msg = malloc(PING_PKT_S-sizeof(struct icmphdr));
 	char *msg;
 };
+
+struct  thread_send_ping_args {
+        int ping_sockfd;
+        struct sockaddr_in *ping_addr;
+        char *ping_dom;
+        char *ping_ip;
+	char *rev_host;
+};
+
+
 
 // Calculating the Check Sum
 unsigned short checksum(void *b, int len)
@@ -122,6 +135,145 @@ char* reverse_dns_lookup(char *ip_addr)
 	ret_buf = (char*)malloc((strlen(buf) +1)*sizeof(char) );
 	strcpy(ret_buf, buf);
 	return ret_buf;
+}
+// make a ping request
+void *thread_send_ping(void *_args){
+//(int ping_sockfd, struct sockaddr_in *ping_addr,char *ping_dom, char *ping_ip, char *rev_host)
+
+
+	struct  thread_send_ping_args *args = (struct thread_send_ping_args *) _args;
+
+	int ping_sockfd = args->ping_sockfd;
+	struct sockaddr_in *ping_addr = args->ping_addr;
+	char *ping_dom = args->ping_dom;
+       	char *ping_ip = args->ping_ip;
+	char *rev_host = args->rev_host;
+
+
+	int ttl_val=64, msg_count=0, i, addr_len, flag=1,
+	    msg_received_count=0;
+
+	struct ping_pkt pckt;
+	struct sockaddr_in r_addr;
+	struct timespec time_start, time_end, tfs, tfe;
+	long double rtt_msec=0, total_msec=0;
+	struct timeval tv_out;
+	tv_out.tv_sec = RECV_TIMEOUT;
+	printf(" RECV_TIMEOUT set to before send %ld\n",tv_out.tv_sec);
+	tv_out.tv_usec = 0;
+
+	clock_gettime(CLOCK_MONOTONIC, &tfs);
+
+
+	// set socket options at ip to TTL and value to 64,
+	// change to what you want by setting ttl_val
+	if (setsockopt(ping_sockfd, SOL_IP, IP_TTL,
+				&ttl_val, sizeof(ttl_val)) != 0){
+		printf("\nSetting socket options to TTL failed!\n");
+		return NULL;
+	}
+	else{
+		printf("\nSocket set to TTL..\n");
+	}
+
+	// setting timeout of recv setting
+	setsockopt(ping_sockfd, SOL_SOCKET, SO_RCVTIMEO,
+			(const char*)&tv_out, sizeof tv_out);
+
+	// send icmp packet in an infinite loop
+	while(pingloop)
+	{
+		// flag is whether packet was sent or not
+		flag=1;
+
+		//filling packet
+		bzero(&pckt, sizeof(pckt));
+
+		pckt.hdr.type = ICMP_ECHO;
+		pckt.hdr.un.echo.id = getpid();
+
+		int packet_size = PING_PKT_S-sizeof(struct icmphdr);
+		//pckt.msg = malloc(PING_PKT_S-sizeof(struct icmphdr));
+		pckt.msg = malloc(packet_size);
+
+		if(pckt.msg == NULL){
+			fputs("memory allocation for msg failed",stderr);
+			exit(EXIT_FAILURE);
+		}
+
+		//for ( i = 0; i < sizeof(pckt.msg)-1; i++ )
+		for ( i = 0; i < packet_size -1; i++ )
+			//pckt.msg[i] = i+'0';
+			pckt.msg[i] = i%10+'0';
+
+		pckt.msg[i] = 0;
+		pckt.hdr.un.echo.sequence = msg_count++;
+		pckt.hdr.checksum = checksum(&pckt, sizeof(pckt));
+
+
+		printf("packet_size=%d\n",packet_size);
+		printf("msg=%s\n",pckt.msg);
+		usleep(PING_SLEEP_RATE);
+		//usleep(1000000);
+
+		//send packet
+		clock_gettime(CLOCK_MONOTONIC, &time_start);
+		if ( sendto(ping_sockfd, &pckt, sizeof(pckt), 0,
+					(struct sockaddr*) ping_addr,
+					sizeof(*ping_addr)) <= 0)
+		{
+			printf("\nPacket Sending Failed!\n");
+			flag=0;
+		}
+
+		//receive packet
+		addr_len=sizeof(r_addr);
+
+		if ( recvfrom(ping_sockfd, &pckt, sizeof(pckt), 0,
+					(struct sockaddr*)&r_addr, &addr_len) <= 0
+				&& msg_count>1){
+			printf("\nPacket receive failed!\n");
+		}else{
+			clock_gettime(CLOCK_MONOTONIC, &time_end);
+
+			double timeElapsed = ((double)(time_end.tv_nsec -
+						time_start.tv_nsec))/1000000.0;
+				rtt_msec = (time_end.tv_sec-
+						time_start.tv_sec) * 1000.0
+				+ timeElapsed;
+
+			// if packet was not sent, don't receive
+			if(flag)
+			{
+				if(!(pckt.hdr.type ==69 && pckt.hdr.code==0))
+				{
+					printf("Error..Packet received with ICMP type %d code %d\n",
+							pckt.hdr.type, pckt.hdr.code);
+				}
+				else
+				{
+					printf("%d bytes from %s (h: %s) (%s) msg_seq=%d ttl=%d rtt = %Lf ms.\n",
+							PING_PKT_S, ping_dom, rev_host,
+							ping_ip, msg_count,
+							ttl_val, rtt_msec);
+
+					msg_received_count++;
+				}
+			}
+		}	
+	}
+	clock_gettime(CLOCK_MONOTONIC, &tfe);
+	double timeElapsed = ((double)(tfe.tv_nsec -
+				tfs.tv_nsec))/1000000.0;
+
+	total_msec = (tfe.tv_sec-tfs.tv_sec)*1000.0+
+		timeElapsed;
+
+	printf("\n===%s ping statistics===\n", ping_ip);
+	printf("\n%d packets sent, %d packets received, %f percent packet loss. Total time: %Lf ms.\n\n",
+			msg_count, msg_received_count,
+			((msg_count - msg_received_count)/msg_count) * 100.0,
+			total_msec);
 }
 
 // make a ping request
@@ -262,8 +414,9 @@ int main(int argc, char *argv[])
 	struct sockaddr_in addr_con;
 	int addrlen = sizeof(addr_con);
 	char net_buf[NI_MAXHOST];
-
 	bool arg_is_valid[argc];
+	int number_of_threads = 0;
+	int threads_counter = 0;
 
 	printf("argc=%d\n",argc);
 	if(argc < 2)
@@ -271,7 +424,6 @@ int main(int argc, char *argv[])
 		printf("\nFormat %s <address> ... <address> -s <size_of_packets> -t <timeout> \n", argv[0]);
 		return 0;
 	}
-
 
 	for(int i=1; i<argc; i++)
 		arg_is_valid[i] = true;
@@ -304,11 +456,17 @@ int main(int argc, char *argv[])
 		}
 	}
 
-	for(int i=1; i<argc; i++)
-	printf("arg%d is %d\n",i,arg_is_valid[i]);
+	for(int i=1; i<argc; i++){
+		printf("arg%d is %d\n",i,arg_is_valid[i]);
+		if(arg_is_valid[i]){
+			number_of_threads++;
+		}
+	}
+
+	pthread_t thread_id[number_of_threads-1];
 
 	for(int i=1; i<argc; i++){
-	
+
 		if(arg_is_valid[i]){
 
 			ip_addr = dns_lookup(argv[i], &addr_con);
@@ -336,10 +494,42 @@ int main(int argc, char *argv[])
 
 			signal(SIGINT, intHandler);//catching interrupt
 
+
 			//send pings continuously
-			send_ping(sockfd, &addr_con, reverse_hostname,
-					ip_addr, argv[i]);
+			//send_ping(sockfd, &addr_con, reverse_hostname,ip_addr, argv[i]);
+
+
+			struct thread_send_ping_args *args = malloc (sizeof (struct thread_send_ping_args));
+			args->ping_sockfd = sockfd;
+			args->ping_addr = &addr_con;
+			args->ping_dom = reverse_hostname;
+			args->ping_ip = ip_addr;
+			args->rev_host = argv[i];
+
+			/*
+
+			   struct  thread_send_ping_args {
+			   int ping_sockfd;
+			   struct sockaddr_in *ping_addr;
+			   char *ping_dom;
+			   char *ping_ip;
+			   char *rev_host;
+			   };
+
+*/
+
+
+
+			pthread_create(&thread_id[threads_counter], NULL, thread_send_ping, args);
+			threads_counter++;
 		}
+	}
+
+	for(int i = 0; i < threads_counter; i++){
+		int result_code = pthread_join(thread_id[i], NULL);
+		assert(!result_code);
+		printf("IN MAIN: Thread %d has ended.\n", i+1);
+
 	}
 
 	return 0;
